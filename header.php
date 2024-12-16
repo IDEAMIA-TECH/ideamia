262d1cb5892503341513429b5287f824f2c302f1
Jorge Plascencia <jorge@ideamia.com.mx>
2024-12-15 18:53:57 -0600

    nuevo menu

--- header.php
+++ header.php
@@ -4,74 +4,21 @@
         <div class="header__logo">
           <a href="#">
             <img class="show-light" src="assets/imgs/logo/logo.png" alt="Site Logo" width="150px">
-            <img class="show-dark" src="assets/imgs/logo/logo.png" alt="Site Logo" width="150px>
+            <img class="show-dark" src="assets/imgs/logo/logo.png" alt="Site Logo" width="150px">
           </a>
         </div>
         <div class="header__nav pos-center">
           <nav class="main-menu">
             <ul>
+            <li><a href="index.php">Inicio</a></li>
+              <li><a href="nosotros.php">Nosotros</a></li>
               <li class="menu-item-has-children">
-                <a href="#">Demos</a>
-                <ul class="dp-menu col-2">
-                  <li><a href="branding-agency.html">Branding Agency</a></li>
-                  <li><a href="web-agency.html">Web Agency</a></li>
-                  <li><a href="seo-agency.html">SEO Agency</a></li>
-                  <li><a href="design-studio.html">Design Agency</a></li>
-                  <li><a href="video-production.html">Video Agency</a></li>
-                  <li><a href="ai-agency.html">AI Agency</a></li>
-                  <li><a href="creative-agency-classic.html">Creative Agency Classic</a></li>
-                  <li><a href="marketing-agency.html">Marketing Agency</a></li>
-                  <li><a href="corporate-agency.html">Corporate Agency</a></li>
-                  <li><a href="startup-agency.html">Startup Agency</a></li>
-                  <li><a href="modern-agency.html">Modern Agency</a></li>
-                  <li><a href="photography-studio.html">Photography Agency</a></li>
-                  <li><a href="creative-agency.html">Creative Agency</a></li>
-                  <li><a href="digital-agency.html">Digital Agency</a></li>
-                </ul>
-              </li>
-              <li><a href="about.html">about</a></li>
-              <li class="menu-item-has-children">
-                <a href="#">portfolio</a>
+                <a href="#">Nuestro Trabajo</a>
                 <ul class="dp-menu">
-                  <li><a href="portfolio-carousel.html">Portfolio Carousel</a></li>
-                  <li><a href="portfolio-material.html">Portfolio Material</a></li>
-                  <li><a href="portfolio-slicer.html">Portfolio Slicer</a></li>
-                  <li><a href="portfolio-spring.html">Portfolio Spring</a></li>
-                </ul>
-              </li>
-              <li class="menu-item-has-children">
-                <a href="#">Pages</a>
-                <ul class="dp-menu">
-                  <li class="menu-item-has-children">
-                    <a href="#">service pages</a>
-                    <ul>
-                      <li><a href="services.html">services</a></li>
-                      <li><a href="service-details.html">service details</a></li>
-                    </ul>
-                  </li>
-                  <li class="menu-item-has-children">
-                    <a href="#">project pages</a>
-                    <ul>
-                      <li><a href="works.html">project</a></li>
-                      <li><a href="work-details.html">project details</a></li>
-                    </ul>
-                  </li>
-                  <li class="menu-item-has-children">
-                    <a href="#">team pages</a>
-                    <ul>
-                      <li><a href="team.html">team</a></li>
-                      <li><a href="team-details.html">team details</a></li>
-                    </ul>
-                  </li>
-                  <li class="menu-item-has-children">
-                    <a href="#">career pages</a>
-                    <ul>
-                      <li><a href="career.html">career</a></li>
-                      <li><a href="career-details.html">career details</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="faq.html">faq</a></li>
-                  <li><a href="404.html">404 page</a></li>
+                  <li><a href="portfolio-carousel.html">Desarrollo</a></li>
+                  <li><a href="portfolio-material.html">Marketing Diigital</a></li>
+                  <li><a href="portfolio-slicer.html">Hosting</a></li>
+                  <li><a href="portfolio-spring.html">Consultoria</a></li>
                 </ul>
               </li>
               <li>
@@ -81,7 +28,7 @@
                   <li><a href="blog-details.html">blog details</a></li>
                 </ul>
               </li>
-              <li><a href="contact.html">Contact</a></li>
+              <li><a href="contact.html">Contacto</a></li>
             </ul>
           </nav>
         </div>
